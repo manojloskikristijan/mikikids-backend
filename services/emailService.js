561f20e3168210932a4d8bae0c81849524a12875
225131 <manojloskikristijan@gmail.com>
2025-09-14 17:58:25 +0200

    nodemailer fix 3

--- services/emailService.js
+++ services/emailService.js
@@ -36,7 +36,7 @@
     class EmailService {
         constructor() {
             this.transporter = nodemailer.createTransport({
-host: "gmail",
+service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS, // Gmail App Password
